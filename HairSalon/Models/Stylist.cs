68600a0e63e2af410fcbe9a07313a3a9eedc4144
JohnNils Olson <johnnils@gmail.com>
2020-07-31 10:49:03 -0700

    fix typo fristname to firstname

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -5,9 +5,8 @@ namespace HairSalon.Models
   public class Stylist
   {
     public int StylistId { get; set; }
-    public string FristName { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
-    public virtual ICollection<Stylist> Stylists { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
     public Stylist()
     {
